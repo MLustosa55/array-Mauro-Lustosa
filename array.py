2ddd521031787b2bd11d3ad7fe030e5e02d4bc73
MLustosa55 <mlustosa551@gmail.com>
2025-09-01 22:46:14 -0300

    Create array.py

---
+++ array.py
@@ -0,0 +1,7 @@
+#lista de nomes
+listaNomes = ["Thamirys, "Mauro", "Igor", "Amanda", "Roni",]
+print ("primeiro nome:", listaNomes [0]
+print ("primeiro nome:", listaNomes [1]
+print ("primeiro nome:", listaNomes [2]
+print ("primeiro nome:", listaNomes [3]
+print ("primeiro nome:", listaNomes [4]
