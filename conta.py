bd06c836cfcc5e27dd38fb228b575f9c0477c1bd
MLustosa55 <mlustosa551@gmail.com>
2025-09-01 22:56:20 -0300

    conta.py

---
+++ conta.py
@@ -0,0 +1,12 @@
+#ContaBancária
+class Titular:
+  def __init__(self, titular, saldo):
+        self.titular = titular
+        self.saldo = saldo
+  def apresentar(self)
+      print f('Olá, {self.titular}! seu saldo é {self.saldo}')
+
+
+cliente1 = Titular ("Mauro", 100)
+cliente1.apresentar()
+  
